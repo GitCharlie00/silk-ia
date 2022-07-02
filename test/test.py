70f15aa978f07259c4a5e62e2251c528c8e2db2c
Claudio Schiavella <schiavella.1884561@studenti.uniroma1.it>
2022-07-02 21:33:35 +0200

    Alcuni test sul dataset con modelli di sklearn

---
+++ test/test.py
@@ -0,0 +1,47 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import svm
+from sklearn import metrics
+
+# Carico il dataset
+path = './data.csv'
+data = pd.read_csv(path, usecols = [i for i in range(5)])
+
+# Creo train e test set
+X_train, X_test, Y_train, Y_test = train_test_split(data[['presenza_foglie','assenza_foglie','presenza_bachi_sfondo','assenza_bachi_sfondo']], data[['classificazione']], test_size=0.13)
+
+# Logistic Regression
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.fit_transform(X_test)
+
+model_lr = LogisticRegression()
+lr = model_lr.fit(X_train, Y_train)
+predictions_lr = model_lr.predict(X_test)
+
+train_score = model_lr.score(X_train, Y_train)
+test_score = model_lr.score(X_test, Y_test)
+print("### LOGISTIC:\n\tAccuracy of training: "+str(train_score)+"\n\tAccuracy of test: "+str(test_score))
+
+# SVM
+clf = svm.SVC(kernel='linear') # Linear Kernel
+svmm = clf.fit(X_train, Y_train)
+predictions_svmm = clf.predict(X_test)
+
+train_score = metrics.accuracy_score(Y_test, predictions_svmm)
+test_score = svmm.score(X_train, Y_train)
+
+print("### SVM:\n\tAccuracy of training: "+str(train_score)+"\n\tAccuracy of test: "+str(test_score))
+
+# K-nearest neighbour
+model_knn = classifier = KNeighborsClassifier(n_neighbors=5)
+knn = model_knn.fit(X_train, Y_train)
+predictions_knn = model_knn.predict(X_test)
+
+train_score = metrics.accuracy_score(Y_test, predictions_knn)
+test_score = knn.score(X_train, Y_train)
+
+print("### KNN:\n\tAccuracy of training: "+str(train_score)+"\n\tAccuracy of test: "+str(test_score))
