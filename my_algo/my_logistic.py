170078b510348c19298241fd24c0ece10306f812
Claudio Schiavella <schiavella.1884561@studenti.uniroma1.it>
2022-07-13 10:53:53 +0200

    Cambiata struttura notebook

--- my_algo/my_logistic.py
+++ my_algo/my_logistic.py
@@ -1,7 +1,4 @@
 import numpy as np
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 
 def logistic(z):
     den = 1 + np.exp(-z)
@@ -24,7 +21,6 @@ def gradient_ascent(X,W,Y,alpha,stop):
     m,n = X.shape
     cost_old = np.inf
     cost_new = cost(W,X,Y)
-    print(cost_old - cost(W,X,Y))
     sum = 0
     iter = 0
 
@@ -35,43 +31,13 @@ def gradient_ascent(X,W,Y,alpha,stop):
             W[0][j] = W[0][j] + (alpha/m) * sum
         cost_old = cost_new
         cost_new = cost(W,X,Y)
-        print(cost_old - cost(W,X,Y))
         iter += 1
 
-    return W, iter  
+    return iter  
 
 def prediction(W,X,Y):
     m = X.shape[0]
-    Y_hat = hyp(X, W) > 0.5
+    h = hyp(X, W)
+    Y_hat = h > 0.6
     accuracy = 1.0/m * np.sum(Y == Y_hat)
-    return accuracy, Y_hat
-
-# Carico il dataset
-path = '../color_extr/data.csv'
-data = pd.read_csv(path, usecols = [i for i in range(5)])
-
-# Creo train e test set
-X_train, X_test, Y_train, Y_test = train_test_split(data[['presenza_foglie','assenza_foglie','presenza_bachi_sfondo','assenza_bachi_sfondo']], data[['classificazione']], test_size=0.13)
-
-m_tr, n_tr = X_train.shape
-X_train = np.concatenate((np.ones((m_tr,1)), X_train), axis=1)
-n_tr +=1
-
-m_te, n_te = X_test.shape
-X_test = np.concatenate((np.ones((m_te,1)), X_test), axis=1)
-n_te += 1
-
-W = np.array(np.zeros((1,n_te)))
-#W = np.matrix(np.random.randn((n_te)))
-
-Y_train = Y_train.to_numpy()
-Y_test = Y_test.to_numpy()
-
-# Normalizzo i dati
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.fit_transform(X_test)
-
-W, iter = gradient_ascent(X_train,W,Y_train,0.04,0.000001)
-print(W,iter)
-print(prediction(W,X_train,Y_train)[0],prediction(W,X_test,Y_test)[0])
+    return accuracy, Y_hat, h
